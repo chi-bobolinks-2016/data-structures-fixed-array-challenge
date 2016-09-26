8d92a7e9121d10b736ffd65b452eb154954fadfb
omgannie <annie_oh@icloud.com>
2016-09-25 20:06:32 -0500

    Implement FixedArray methods

--- fixed_array.rb
+++ fixed_array.rb
@@ -1,3 +1,16 @@
 class FixedArray
+  attr_accessor :array
 
+  def initialize(size)
+    @array = Array.new(size)
+  end
+
+  def get(index)
+    self.array[index]
+  end
+
+  def set(index, element)
+    raise IndexError if index.to_i >= self.array.length
+    self.array[index] = element
+  end
 end
--- fixed_array_spec.rb
+++ fixed_array_spec.rb
@@ -1,5 +1,21 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+  let(:my_array) { FixedArray.new(3) }
+
+  it "initializes a new FixedArray object" do
+    expect(my_array.class).to eq FixedArray
+  end
+
+  it "initializes a new object with an array of 3 empty spaces" do
+    expect(my_array.array).to eq [nil, nil, nil]
+  end
+
+  it "gets the value at an index of the array" do
+    expect(my_array.get(0)).to eq nil
+  end
+
+  it "sets the value at an index of the array" do
+    expect(my_array.set(0, "my element")).to eq "my element"
+  end
 end
